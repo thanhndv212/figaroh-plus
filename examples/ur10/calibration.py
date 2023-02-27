db64581dd4d3f8a44d812adfbdb0c78ef7ef526a
Florent Lamiraux <florent@laas.fr>
2023-02-27 16:57:42 +0100

    [License] Change to Apache 2.

--- examples/ur10/calibration.py
+++ examples/ur10/calibration.py
@@ -1,3 +1,17 @@
+# Copyright [2022-2023] [CNRS, Toward SAS]
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from os.path import dirname, join, abspath
 
@@ -15,7 +29,6 @@ import yaml
 from yaml.loader import SafeLoader
 import pprint
 
-from figaroh.meshcat_viewer_wrapper import MeshcatVisualizer
 from figaroh.tools.robot import Robot
 from figaroh.calibration.calibration_tools import (
     get_param_from_yaml,
