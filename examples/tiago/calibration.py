641ef29f5184f7c33d41b76a7c6ca2f6ac2de0ad
thanhndv212 <thanhndv212@gmail.com>
2023-12-07 10:01:37 +0100

    Parse argument from cmd line

--- examples/tiago/calibration.py
+++ examples/tiago/calibration.py
@@ -13,13 +13,46 @@
 # limitations under the License.
 
 from tiago_tools import TiagoCalibration, load_robot, write_to_xacro
-# if load_by_urdf=True, make sure to create a correct urdf for the robot
-tiago = load_robot("data/urdf/tiago_48_hey5.urdf", load_by_urdf=False)
-# del_list=[(0, 1)], 0: numbered marker, 1: numbered sample will be removed
-tiago_calib = TiagoCalibration(tiago, "config/tiago_config.yaml", del_list=[])
-tiago_calib.initialize()
-tiago_calib.solve()
-write_to_xacro(
-    tiago_calib, file_name="tiago_master_calibration.yaml", file_type="yaml"
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="parse calibration setups", add_help=False
+    )
+    parser.add_argument(
+        "-e", "--end_effector", default="hey5", dest="end_effector"
+    )
+    parser.add_argument(
+        "-u", "--load_by_urdf", default=True, dest="load_by_urdf"
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+args = parse_args()
+
+# load_by_urdf = False, load robot from rospy.get_param(/robot_description)
+tiago = load_robot(
+    "data/urdf/tiago_48_{}.urdf".format(args.end_effector), load_by_urdf=True
 )
-tiago_calib.plot()
+
+# create a calibration object from config file
+# del_list=[(0, 1)], 0: numbered marker, 1: numbered sample will be removed
+tiago_calib = TiagoCalibration(
+    tiago, "config/tiago_config_{}.yaml".format(args.end_effector), del_list=[]
+)
+
+# load data file and determine parameters to be calibrated
+tiago_calib.initialize()
+
+# solve least_squares estimation
+tiago_calib.solve()
+
+write_to_xacro(
+    tiago_calib,
+    file_name="tiago_master_calibration_{}.yaml".format(args.end_effector),
+    file_type="yaml",
+)
+tiago_calib.plot(lvl=1)
