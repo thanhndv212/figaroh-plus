e4f3d43e4a9c682d9b729ea7380ecbd7ca6103fa
thanhndv212 <thanhndv212@gmail.com>
2024-03-06 09:03:44 +0100

    reorg

--- examples/tiago/calibration.py
+++ examples/tiago/calibration.py
@@ -17,34 +17,26 @@ from tiago_utils.robot_tools import (
     load_robot,
     write_to_xacro,
 )
-import argparse
 from os.path import abspath
 
 
-# load_by_urdf, otherwise from rospy.get_param(/robot_description)
-tiago = load_robot(abspath("urdf/tiago_48_schunk.urdf"), load_by_urdf=True)
-
-# del_list=[(0, 1)], 0: numbered marker, 1: numbered sample will be removed
-tiago_calib = RobotCalibration(
-    tiago, abspath("config/tiago_config_mocap_vicon.yaml"), del_list=[]
-)
-
+tiago = load_robot(abspath("urdf/tiago_48_schunk.urdf"))
+tiago_calib = RobotCalibration(tiago, abspath("config/tiago_config_mocap_vicon.yaml"))
 tiago_calib.initialize()
-torso_idxq = tiago_calib.param["config_idx"][0]
+# tiago_calib.param["param_name"].remove('d_pz_arm_2_joint')
 tiago_calib.solve()
 tiago_calib.plot(lvl=1)
-# load absolute encoder values
-tiago_abs = RobotCalibration(
-    tiago, abspath("config/tiago_config_mocap_vicon_abs.yaml"), del_list=[]
-)
-tiago_abs.initialize()
 
+# load absolute encoder values
+# torso_idxq = tiago_calib.param["config_idx"][0]
+# tiago_abs = RobotCalibration(
+#     tiago, abspath("config/tiago_config_mocap_vicon_abs.yaml"), del_list=[]
+# )
+# tiago_abs.initialize()
 # replace relative encoder values of torso to absolute one
 # tiago_abs.q_measured[:, torso_idxq] = tiago_calib.q_measured[:, torso_idxq]
-# tiago_calib.param["param_name"].remove('d_pz_arm_2_joint')
-tiago_abs.solve()
-tiago_abs.plot(lvl=1)
-
+# tiago_abs.solve()
+# tiago_abs.plot(lvl=1)
 # write_to_xacro(
 #     tiago_calib,
 #     file_name="tiago_master_calibration_{}.yaml".format(args.end_effector),
