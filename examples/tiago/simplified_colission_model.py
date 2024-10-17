a972aae3d580fd691d0ca2628243b3288263392b
thanhndv212 <thanhndv212@gmail.com>
2024-10-17 23:03:02 +0200

    [Tiago] Fix dummy collision objects

--- examples/tiago/simplified_colission_model.py
+++ examples/tiago/simplified_colission_model.py
@@ -13,19 +13,13 @@
 # limitations under the License.
 
 import numpy as np
-from numpy import pi
 import hppfcl
 import pinocchio as pin
 import time
 
-from figaroh.tools.robot import Robot
-# from figaroh.tools.regressor import *
-# from figaroh.tools.qrdecomposition import *
-# from figaroh.tools.randomdata import *
-from figaroh.tools.robotcollisions import *
-from figaroh.meshcat_viewer_wrapper import MeshcatVisualizer
+from figaroh.tools.robotcollisions import CollisionWrapper
+from figaroh.meshcat_viewer_wrapper.visualizer import MeshcatVisualizer
 
-import os
 from os.path import dirname, join, abspath
 
 
@@ -58,8 +52,8 @@ def Sphere(name, joint, radius, placement, color=[0.7, 0.7, 0.98, 1]):
 
 
 def build_tiago_simplified(robot):
-    visual_model = robot.visual_model
-    geom_model = robot.geom_model
+    # visual_model = robot.visual_model
+    # geom_model = robot.robot.geom_model
 
     xyz_1 = np.array([-0.028, 0, -0.01])
     xyz_2 = np.array([0.005, 0, -0.35])
@@ -67,39 +61,96 @@ def build_tiago_simplified(robot):
     xyz_4 = np.array([0, 0, 0])
     xyz_5 = np.array([-0.03, 0.09, 0])
 
-    geom_model.addGeometryObject(
+    robot.geom_model.addGeometryObject(
         Box("torso_up_box", 1, 0.28, 0.35, 0.2, pin.SE3(np.eye(3), xyz_1))
     )
-    visual_model.addGeometryObject(
-        Box("torso_up_box", 1, 0.28, 0.35, 0.2, pin.SE3(np.eye(3), xyz_1))
+    robot.visual_model.addGeometryObject(
+        Box(
+            "torso_up_box",
+            robot.model.getJointId("torso_lift_joint"),
+            0.28,
+            0.35,
+            0.2,
+            pin.SE3(np.eye(3), xyz_1),
+        )
     )
-    geom_model.addGeometryObject(
-        Box("torso_low_box", 1, 0.221, 0.26, 0.35, pin.SE3(np.eye(3), xyz_2))
+    robot.geom_model.addGeometryObject(
+        Box(
+            "torso_low_box",
+            robot.model.getJointId("torso_lift_joint"),
+            0.221,
+            0.26,
+            0.35,
+            pin.SE3(np.eye(3), xyz_2),
+        )
     )
-    visual_model.addGeometryObject(
-        Box("torso_low_box", 1, 0.221, 0.26, 0.35, pin.SE3(np.eye(3), xyz_2))
+    robot.visual_model.addGeometryObject(
+        Box(
+            "torso_low_box",
+            robot.model.getJointId("torso_lift_joint"),
+            0.221,
+            0.26,
+            0.35,
+            pin.SE3(np.eye(3), xyz_2),
+        )
     )
-    geom_model.addGeometryObject(
-        Capsule("base_cap", 0, 0.3, 0.25, pin.SE3(np.eye(3), xyz_3))
+    robot.geom_model.addGeometryObject(
+        Capsule(
+            "base_cap",
+            robot.model.getJointId("universe"),
+            0.3,
+            0.25,
+            pin.SE3(np.eye(3), xyz_3),
+        )
     )
-    visual_model.addGeometryObject(
-        Capsule("base_cap", 0, 0.3, 0.25, pin.SE3(np.eye(3), xyz_3))
+    robot.visual_model.addGeometryObject(
+        Capsule(
+            "base_cap",
+            robot.model.getJointId("universe"),
+            0.3,
+            0.25,
+            pin.SE3(np.eye(3), xyz_3),
+        )
     )
-    geom_model.addGeometryObject(
-        Capsule("forearm_cap", 6,  0.30, 0.10,pin.SE3(np.eye(3), xyz_4))
+    robot.geom_model.addGeometryObject(
+        Capsule(
+            "forearm_cap",
+            robot.model.getJointId("arm_5_joint"),
+            0.10,
+            0.40,
+            pin.SE3(np.eye(3), xyz_4),
+        )
     )
-    visual_model.addGeometryObject(
-        Capsule("forearm_cap", 6,  0.10, 0.30, pin.SE3(np.eye(3), xyz_4))
+    robot.visual_model.addGeometryObject(
+        Capsule(
+            "forearm_cap",
+            robot.model.getJointId("arm_5_joint"),
+            0.10,
+            0.40,
+            pin.SE3(np.eye(3), xyz_4),
+        )
     )
-    geom_model.addGeometryObject(
-        Capsule("head_cap", 10, 0.17, 0.25, pin.SE3(np.eye(3), xyz_5))
+    robot.geom_model.addGeometryObject(
+        Capsule(
+            "head_cap",
+            robot.model.getJointId("head_2_joint"),
+            0.17,
+            0.25,
+            pin.SE3(np.eye(3), xyz_5),
+        )
     )
-    visual_model.addGeometryObject(
-        Capsule("head_cap", 10, 0.17, 0.25, pin.SE3(np.eye(3), xyz_5))
+    robot.visual_model.addGeometryObject(
+        Capsule(
+            "head_cap",
+            robot.model.getJointId("head_2_joint"),
+            0.17,
+            0.25,
+            pin.SE3(np.eye(3), xyz_5),
+        )
     )
 
-    # for k in range(len(geom_model.geometryObjects)):
-    #     print("object number %d" % k, geom_model.geometryObjects[k].name)
+    for k in range(len(robot.geom_model.geometryObjects)):
+        print("object number %d" % k, robot.geom_model.geometryObjects[k].name)
 
     arm_link_names = [
         "forearm_cap"
@@ -109,21 +160,22 @@ def build_tiago_simplified(robot):
         # "wrist_ft_link_0",
         # "wrist_ft_tool_link_0",
     ]
-    arm_link_ids = [geom_model.getGeometryId(k) for k in arm_link_names]
+    arm_link_ids = [robot.geom_model.getGeometryId(k) for k in arm_link_names]
     mask_link_names = [
         "torso_up_box",
         "torso_low_box",
         "base_cap",
         "head_cap",
     ]
-    mask_link_ids = [geom_model.getGeometryId(k) for k in mask_link_names]
+    mask_link_ids = [robot.geom_model.getGeometryId(k) for k in mask_link_names]
     for i in mask_link_ids:
         for j in arm_link_ids:
-            geom_model.addCollisionPair(pin.CollisionPair(i, j))
+            robot.geom_model.addCollisionPair(pin.CollisionPair(i, j))
     print("number of collision pairs of simplified model is: ",
-          len(geom_model.collisionPairs))
+          len(robot.geom_model.collisionPairs))
 
     return robot
+
 
 def build_tiago_normal(robot):
     # # Remove collision pairs listed in the SRDF file
@@ -147,19 +199,20 @@ def main():
     time.sleep(1)
     from tiago_tools import load_robot
     # 1/ Load robot model and create a dictionary containing reserved constants
-    robot = load_robot("data/urdf/tiago_hey5.urdf")
+    robot = load_robot("data/urdf/tiago_48_hey5.urdf")
     robot = build_tiago_simplified(robot)
     collision = CollisionWrapper(robot, viz=None)
 
     def check_collision(collision, q):
         is_collision = collision.computeCollisions(q)
-        # collision.getAllpairs()
+        collision.getAllpairs()
         if not is_collision:
             print("self-collision is not violated!")
         else:
             print("self-collision is violated!")
         return is_collision
     # TODO: write for checking collision model and collision data
+
     print(robot.model)
     viz = MeshcatVisualizer(
         model=robot.model, collision_model=robot.collision_model, visual_model=robot.visual_model, url='classical'
@@ -167,9 +220,11 @@ def main():
     time.sleep(3)
     for i in range(20):
         q = pin.randomConfiguration(robot.model)
-        if not check_collision(collision, q):
-            viz.display(q)
-            time.sleep(0.5)
+        # if not check_collision(collision, q):
+        check_collision(collision, q)
+        collision.displayCollisions()
+        viz.display(q)
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
